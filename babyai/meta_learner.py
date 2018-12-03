785ea7f658a42289b24e6e46bfcb414a87e60e75
Kanishk Gandhi <kanishk050296@gmail.com>
2018-12-02 21:14:59 -0500

    add learner initialization

--- babyai/meta_learner.py
+++ babyai/meta_learner.py
@@ -1,13 +1,18 @@
 import torch
 import torch.nn as nn
+from imitation import ImitationLearning
+import utils
+import meta_utils
 
 class MetaLearner(nn.Module):
     """ Bare Meta-learner class
         Should be added: intialization, hidden states, more control over everything
     """
-    def __init__(self, model):
+    def __init__(self, model, args):
         super(MetaLearner, self).__init__()
         self.weights = Parameter(torch.Tensor(1, 2))
+        self.args = args
+        self._init_train()
 
     def forward(self, forward_model, backward_model):
         """ Forward optimizer with a simple linear neural net
@@ -29,3 +34,45 @@ class MetaLearner(nn.Module):
             # Update backward_model (meta-gradients can flow) and forward_model (no need for meta-gradients).
             module_b._parameters[name_b] = param_b
             param_f.data = param_b.data
+
+    def _init_train(args):
+        if args.multi_env is not None:
+            assert len(args.multi_demos) == len(args.multi_episodes)
+
+        args.model = args.model or ImitationLearning.default_model_name(args)
+        utils.configure_logging(args.model)
+        logger = logging.getLogger(__name__)
+
+        self.il_learn_forward = ImitationLearning(args)
+        self.il_learn_backward = ImitationLearning(args)
+
+        # Define logger and Tensorboard writer
+        self.header = (["update", "frames", "FPS", "duration", "entropy", "policy_loss", "train_accuracy"]
+                  + ["validation_accuracy"])
+        if args.multi_env is None:
+            self.header.extend(["validation_return", "validation_success_rate"])
+        else:
+            self.header.extend(["validation_return_{}".format(env) for env in args.multi_env])
+            self.header.extend(["validation_success_rate_{}".format(env) for env in args.multi_env])
+        writer = None
+        if args.tb:
+            from tensorboardX import SummaryWriter
+            writer = SummaryWriter(utils.get_log_dir(args.model))
+
+        # Define csv writer
+        selof.csv_writer = None
+        self.csv_path = os.path.join(utils.get_log_dir(args.model), 'log.csv')
+        first_created = not os.path.exists(self.csv_path)
+        # we don't buffer data going in the csv log, cause we assume
+        # that one update will take much longer that one write to the log
+        self.csv_writer = self.csv.writer(open(self.csv_path, 'a', 1))
+        if first_created:
+            self.csv_writer.writerow(self.header)
+
+        # Get the status path
+        self.status_path = os.path.join(utils.get_log_dir(args.model), 'status.json')
+
+        # Log command, availability of CUDA, and model
+        logger.info(args)
+        logger.info("CUDA available: {}".format(torch.cuda.is_available()))
+        logger.info(self.il_learn.acmodel)
